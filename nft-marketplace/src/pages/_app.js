aa6ade500d80dfe7961366578f7b4e68d9202e1d
byobelixasterix <deepakbhatia@users.noreply.github.com>
2023-04-24 14:45:19 +0000

    remve <a

--- nft-marketplace/src/pages/_app.js
+++ nft-marketplace/src/pages/_app.js
@@ -9,24 +9,16 @@ function MyApp({ Component, pageProps }) {
         <p className="text-4xl font-bold">Metaverse Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
               Home
-            </a>
           </Link>
           <Link href="/create-nft">
-            <a className="mr-6 text-pink-500">
               Sell NFT
-            </a>
           </Link>
           <Link href="/my-nfts">
-            <a className="mr-6 text-pink-500">
               My NFTs
-            </a>
           </Link>
           <Link href="/dashboard">
-            <a className="mr-6 text-pink-500">
               Dashboard
-            </a>
           </Link>
         </div>
       </nav>
